c461c538e15c9ebca94308e9707fc0b3a7e0d8da
qhawe <qhawe@kingston-enterprises.net>
2023-01-31 21:18:46 +0200

    mended database migrations directory

--- Database.php
+++ Database.php
@@ -26,14 +26,14 @@ class Database
         $appliedMigrations = $this->getAppliedMigrations();
 
         $newMigrations = [];
-        $files = scandir(Application::$ROOT_DIR . '/migrations');
+        $files = scandir(Application::$ROOT_DIR . '/../migrations');
         $toApplyMigrations = array_diff($files, $appliedMigrations);
         foreach ($toApplyMigrations as $migration) {
             if ($migration === '.' || $migration === '..') {
                 continue;
             }
 
-            require_once Application::$ROOT_DIR . '/migrations/' . $migration;
+            require_once Application::$ROOT_DIR . '/../migrations/' . $migration;
             $className = pathinfo($migration, PATHINFO_FILENAME);
             $instance = new $className();
             $this->log("Applying migration $migration");
